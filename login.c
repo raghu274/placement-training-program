6fbc64e8d39c9a26ee0d03734f194f58825b1113
raghu274 <raghusudharshan1001@gmail.com>
2025-05-15 10:14:03 +0530

    Add files via upload

---
+++ login.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+char user[20]='raghu';
+int password = 12345;
+char arr[20];
+scanf("%s",arr);
+scanf("%d",&x);
+if((strcmp(username,arr)==0)&&(password==x))printf("login successfully");
+
+    return 0;
+}
