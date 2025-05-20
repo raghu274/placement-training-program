78979cef91a5f07e3d2bf7f74ac95c134208d701
raghu274 <raghusudharshan1001@gmail.com>
2025-05-20 10:17:04 +0530

    Add files via upload

---
+++ day_06/reverse element in arr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int size, sum=0;
+   scanf("%d",&size);
+   int arr[size];
+   for(int i=0;i<size;i++){
+       scanf("%d",&arr[i]);
+       sum+=arr[i];
+   }
+       float avg=(float)sum/size;
+       printf("sum=%d avg=%.2f",sum,avg);
+   
+}
